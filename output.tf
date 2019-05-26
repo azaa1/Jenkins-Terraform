0b80d5cf6b608291ff702727b8444749fa75be0b
azaa1 <azaa.chi11@gmail.com>
2019-05-25 22:24:18 -0500

    output.tf modified

--- output.tf
+++ output.tf
@@ -3,7 +3,5 @@ output "Public ip" {
   value            = "${digitalocean_droplet.dev.ipv4_address}"
   description      = "The Public IP of the Server"
 
-  depends_on = [
-    digitalocean_droplet.test,
-  ]
+  depends_on = ["digitalocean_droplet.test"]
 }
